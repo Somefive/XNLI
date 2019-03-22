d2f0e675b1cfed758977b27364656d38d8649042
Somefive <Somefive@foxmail.com>
2019-03-22 13:58:54 -0400

    use gelu in original gelu paper

--- src/transformer.py
+++ src/transformer.py
@@ -8,7 +8,7 @@ import torch.nn.functional as F
 
 
 def gelu(x):
-    return 0.5 * x * (1.0 + torch.erf(x / np.sqrt(2.0)))
+    return 0.5 * x * (1 + torch.tanh(math.sqrt(2 / math.pi) * (x + 0.044715 * torch.pow(x, 3))))
 
 
 class MultiHeadAttention(nn.Module):
