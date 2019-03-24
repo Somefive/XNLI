78f9e3c040f935333a7e6a6e91a343990e6173ae
Xinya Li <xinyal@andrew.cmu.edu>
2019-03-23 22:49:11 -0400

    Create install-tools.sh
    Is working. Need to manually install the Stanford tokenizer for Chinese/Thai. Check back later(?)

---
+++ redo/install-tools.sh
@@ -0,0 +1,36 @@
+
+set -e
+# lg=$1  # input language
+
+# data path
+MAIN_PATH=$PWD
+TOOLS_PATH=$PWD/tools
+
+# tools
+MOSES_DIR=$TOOLS_PATH/mosesdecoder
+mkdir -p $TOOLS_PATH
+
+#
+# Download and install tools
+#
+
+cd $TOOLS_PATH
+# Download Moses
+if [ ! -d "$MOSES_DIR" ]; then
+  echo "Cloning Moses from GitHub repository..."
+  git clone https://github.com/moses-smt/mosesdecoder.git
+fi
+
+# Chinese segmenter
+if ! ls $TOOLS_PATH/stanford-segmenter-* 1> /dev/null 2>&1; then
+   echo "Stanford segmenter not found at $TOOLS_PATH/stanford-segmenter-*"
+   echo "Please install Stanford segmenter in $TOOLS_PATH"
+   exit 1
+fi
+ 
+# # Thai tokenizer
+if ! python -c 'import pkgutil; exit(not pkgutil.find_loader("pythainlp"))'; then
+   echo "pythainlp package not found in python"
+   echo "Please install pythainlp (pip install pythainlp)"
+   exit 1
+ fi
