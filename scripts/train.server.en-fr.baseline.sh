d32ac7ae78005b74ae0f433b261403c76d9e5fdc
Somefive <somefive@foxmail.com>
2019-03-23 23:54:45 -0400

    add standard size model for tlm; good

--- scripts/train.server.en-fr.baseline.sh
+++ scripts/train.server.en-fr.baseline.sh
@@ -1,38 +1,38 @@
 #!/biN/BASH
 python src/train.py \
---load_path model/en-fr-ex \
---save_path model/en-fr-ex \
+--load_path model/en-fr-baseline \
+--save_path model/en-fr-tune \
 --save_path_inc 0 \
 --vocab_size 20400 \
 --max_seq_len 256 \
---batch_size 128 \
+--batch_size 64 \
 --shuffle True \
 --num_workers 12 \
 \
---model_n_layers 6 \
---model_dim 512 \
---model_d_ff 1024 \
+--model_n_layers 4 \
+--model_dim 256 \
+--model_d_ff 512 \
 --model_dropout 0.1 \
 --model_heads 8 \
 --model_encoder_only True \
 \
 --max_epoch 100 \
---epoch_size 2000 \
+--epoch_size 5000 \
 --print_interval 1 \
 --verbose True \
 \
 --mlm_train_set_size 1000000 \
 --mlm_valid_set_size 5000 \
---tlm_train_set_size 10000000 \
+--tlm_train_set_size 1000000 \
 --tlm_valid_set_size 5000 \
 --xnli_train_set_size 400000 \
 --xnli_valid_set_size 2500 \
 \
---tlm True \
+--xnli True \
 \
 --vocab_path en fr \
---mlm_train_paths en \
---mlm_valid_paths en \
+--mlm_train_paths en fr \
+--mlm_valid_paths en fr \
 --tlm_train_paths en-fr.en en-fr.fr \
 --tlm_valid_paths en-fr.en en-fr.fr \
 --xnli_langs en fr \
@@ -41,4 +41,4 @@ python src/train.py \
 --device cuda:0 \
 --gpus 0,1,2 \
 --fp16 False \
---lr 2e-4
+--lr 2e-5
